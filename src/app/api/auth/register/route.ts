1c68d50335c17fe4da0d31c29a124541f4d1b34f
tkdgns07 <tkdgns070122@gmail.com>
2025-01-07 11:08:31 +0900

    ddd

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,8 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "#/prisma/client";
+import prisma from "../../../../../prisma/client";
 import bcrypt from "bcrypt";
 
-// 무작위 색상을 생성하는 함수
 const getRandomColor = () => {
   const letters = "0123456789ABCDEF";
   let color = "#";
@@ -12,7 +11,6 @@ const getRandomColor = () => {
   return color;
 };
 
-// SVG로 프로필 이미지 생성 함수
 const generateRandomAvatar = (): string => {
   const color1 = getRandomColor();
   const color2 = getRandomColor();
@@ -30,19 +28,16 @@ const generateRandomAvatar = (): string => {
   `;
 };
 
-// 이메일 형식 검증 함수
 const emailPattern = /^h012s24\d{3}@gw1\.kr$/;
 function isValidEmail(email: string) {
   return emailPattern.test(email);
 }
 
-// POST 메서드 핸들러
 export async function POST(request: NextRequest) {
   try {
     const body = await request.json();
     const { name, email, password } = body;
 
-    // 이메일 검증
     if (!isValidEmail(email)) {
       return NextResponse.json({ error: "Invalid email format" }, { status: 400 });
     }
@@ -57,20 +52,17 @@ export async function POST(request: NextRequest) {
       return NextResponse.json({ error: "Existing Name" }, { status: 400 });
     }
 
-    // 비밀번호 해싱
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // 아바타 생성 (Base64)
     const avatarSVG = generateRandomAvatar();
     const avatarBase64 = Buffer.from(avatarSVG).toString("base64");
 
-    // 사용자 생성
     const user = await prisma.user.create({
       data: {
         name,
         email,
         password: hashedPassword,
-        image: `data:image/svg+xml;base64,${avatarBase64}`, // Base64로 저장
+        image: `data:image/svg+xml;base64,${avatarBase64}`,
       },
     });
 
